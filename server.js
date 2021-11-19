5acb962f34328987a627d50f9cd41fc846cd43ab
Elliott <elliottchong16@gmail.com>
2021-11-19 20:45:18 +0800

    fix nodemon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.json("API Running"));
+app.get("/", (req, res) => res.json({ elle: "is running" }));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
