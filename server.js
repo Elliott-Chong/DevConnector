147bd76c1cec5966c0a520da2e2c9aaa0e04f65d
Elliott <elliottchong16@gmail.com>
2021-11-21 18:23:38 +0800

    another commit before botanic gardens

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json({ extended: false }));
+
+connectDB();
+
 app.get("/", (req, res) => res.json({ elle: "is running" }));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
