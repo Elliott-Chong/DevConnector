cfe8f301965c642c7ae9a508782827763fca203e
Elliott <elliottchong16@gmail.com>
2021-11-23 21:56:15 +0800

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
-// @route  GET api/peofile
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+// @route  GET api/profile/me
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => res.json("Profile route"));
+router.get("/", auth, async (req, res) => {
+  const id = req.user.id;
+  try {
+    const profile = await Profile.findOne({ user: id }).populate("user");
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(200).json({ profile });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Server Error");
+  }
+});
 
 module.exports = router;
