3c02979ec98babed9c65e0dde2ea8701b02b5429
Elliott <elliottchong16@gmail.com>
2021-12-01 20:50:40 +0800

    fix

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Experience = ({ experiences }) => {
+  return (
+    <>
+      <h2 className="my-2">Experience Credentials</h2>
+    </>
+  );
+};
+
+export default Experience;
