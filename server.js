b67612a4927280b69a4c2d7ce13f5597414e9282
Elliott <elliottchong16@gmail.com>
2021-12-05 21:10:50 +0800

    Initial Deploy

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
