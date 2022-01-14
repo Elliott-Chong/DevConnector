6eea80c27bf02792085dad03a8c4823e34de2cd6
Elliott Chong <elliottchong16@gmail.com>
2022-01-14 23:15:43 +0800

    change default axios domain

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import AppProvider from "./context/context";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = "https://www.elliott-project.com";
+axios.defaults.baseURL = "https://www.elliott-project.com:444";
 
 ReactDOM.render(
   <AppProvider>
--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(function (req, res, next) {
 
 connectDB();
 
-app.get("/", (req, res) => res.json({'hello': 'world'}));
+app.get("/", (req, res) => res.send('DevConnector API running'));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-app.listen(process.env.PORT || PORT);
+app.listen(process.env.PORT || PORT, ()=>console.log('DevConnector API running on http://localhost:5000'));
